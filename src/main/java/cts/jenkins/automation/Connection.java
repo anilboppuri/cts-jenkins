1191b6b50e54804a860e38496353399ba1e29035
anilboppuri <anil4java2@gmail.com>
2021-02-25 10:59:29 +0530

    added a new connection class

---
+++ src/main/java/cts/jenkins/automation/Connection.java
@@ -0,0 +1,10 @@
+package cts.jenkins.automation;
+
+public class Connection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test connection");
+	}
+
+}
